670eb0d3f4f847b0af2234c0e81644f95c667c23
Rodrigo Viturino <Viturino_souza@outlook.com>
2020-03-27 19:22:29 -0300

    Header - Adicionado menu sticky

--- src/js/theme.js
+++ src/js/theme.js
@@ -1,12 +1,12 @@
-let menu = document.querySelector('.header');
-let headerClassList = menu.classList
+// let menu = document.querySelector('.header__rowBottom');
+// let headerClassList = menu.classList
 
-window.addEventListener('scroll', () => {
-    if (window.scrollY >= 100) {
-        if(!headerClassList.contains('scrollHide')) {
-            headerClassList.add('scrollHide')
-        }
-    } else {
-        headerClassList.remove('scrollHide');
-    }
-});
+// window.addEventListener('scroll', () => {
+//     if (window.scrollY >= 100) {
+//         if(!headerClassList.contains('scrollHide')) {
+//             headerClassList.add('scrollHide')
+//         }
+//     } else {
+//         headerClassList.remove('scrollHide');
+//     }
+// });
